c46941b636b5a449694e74e294f08514bae36211
ZsZs <zsolt.zsuffa@gmail.com>
2012-03-02 08:47:10 +0100

    Set isInitialized to true after lazyInitialization.

--- Implementation/DomainTier/Source/hu/itkodex/litest/fixture/GenericCompositeFixture.java
+++ Implementation/DomainTier/Source/hu/itkodex/litest/fixture/GenericCompositeFixture.java
@@ -74,6 +74,7 @@ public abstract class GenericCompositeFixture<S> extends GenericTestFixture<S> i
    protected void lazyInitialization() {
       if( !isInitialized ) {
          defineComponentTypes();
+         isInitialized = true;
       }
    }
 }
